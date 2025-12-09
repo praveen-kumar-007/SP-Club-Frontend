31d29da8c8ebd26834c44aa997370586e695cb8b
praveen-kumar-007 <praveen.pr105@gmail.com>
2025-12-09 23:05:48 +0530

    Add Vercel Analytics for web performance monitoring

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,5 @@
 import { createRoot } from "react-dom/client";
+import { Analytics } from "@vercel/analytics/react";
 import App from "./App.tsx";
 import "./index.css";
 import { HelmetProvider } from "react-helmet-async";
@@ -6,5 +7,6 @@ import { HelmetProvider } from "react-helmet-async";
 createRoot(document.getElementById("root")!).render(
 	<HelmetProvider>
 		<App />
+		<Analytics />
 	</HelmetProvider>
 );
